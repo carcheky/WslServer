5ff48982c7e554fc8276137b963be275509dee6a
carcheky <carcheky@gmail.com>
2020-03-25 17:15:31 +0100

    sererudpated

--- INSTALLER/INSTALL_SERVER.bat
+++ INSTALLER/INSTALL_SERVER.bat
@@ -1,7 +1,17 @@
-echo %CD%
+@echo off
+echo  called:  %0
+echo  drive letter:  %~d0
+echo  path without the drive letter:  %~p0
+echo  filename:  %~n0
+echo  filename's suffix:  %~x0
+echo  file attributes:  %~a0
+echo  date+time:  %~t0
+echo  filesize:  %~z0
+echo  fully qualified path+filename:  %~dpnx0
 
 
-wsl --distribution WslServer --user user "echo hola"
+
+wsl --distribution WslServer --user user cd %~dpnx0 && pwd
 
 
 pause

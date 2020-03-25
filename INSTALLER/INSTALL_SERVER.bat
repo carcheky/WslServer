ef6a579782f5dfed3a79340bc90de7a93a3e5dfe
carcheky <carcheky@gmail.com>
2020-03-25 21:23:47 +0100

    ccktools

--- INSTALLER/INSTALL_SERVER.bat
+++ INSTALLER/INSTALL_SERVER.bat
@@ -1,17 +1,34 @@
 @echo off
-echo  called:  %0
-echo  drive letter:  %~d0
-echo  path without the drive letter:  %~p0
-echo  filename:  %~n0
-echo  filename's suffix:  %~x0
-echo  file attributes:  %~a0
-echo  date+time:  %~t0
-echo  filesize:  %~z0
-echo  fully qualified path+filename:  %~dpnx0
+
+REM echo  called:  %0
+REM echo  drive letter:  %~d0
+REM echo  path without the drive letter:  %~p0
+REM echo  filename:  %~n0
+REM echo  filename's suffix:  %~x0
+REM echo  file attributes:  %~a0
+REM echo  date+time:  %~t0
+REM echo  filesize:  %~z0
+REM echo  fully qualified path+filename:  %~dpnx0
 
 
+SET DRIVE_LETTER=%~d0
+SET CDIR=%~p0
+SET PARENT_CDIR=%CDIR:~1,-1%
+SET LINUX_PATH=/mnt/c/wsl/
 
-wsl --distribution WslServer --user user cd %~dpnx0 && pwd
+@echo on
+
+echo path: %~0
+
+echo DRIVE_LETTER: %DRIVE_LETTER%
+echo PARENT_CDIR: %PARENT_CDIR%
+echo LINUX_PATH: %LINUX_PATH%
+
+
+@echo off
+
+
+wsl --distribution WslServer --user user 'cd %LINUX_PATH% && pwd'
 
 
 pause

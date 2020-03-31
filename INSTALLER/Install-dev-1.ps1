873edeefcf4158b2b5bb7147143f6d2bce49ff5f
carcheky <carcheky@gmail.com>
2020-03-31 11:07:10 +0200

    INSTALLER/Install-dev-1: create

---
+++ INSTALLER/Install-dev-1.ps1
@@ -0,0 +1,24 @@
+﻿$TARGET_DIR=$env:APPDATA+'\WslServer'
+echo $TARGET_DIR
+
+wsl --unregister WslServer
+if( $TARGET_DIR -eq $env:APPDATA+'\WslServer' ){
+    rmdir /s /q $TARGET_DIR
+}
+
+curl https://aka.ms/wsl-ubuntu-1804 -L -o install.zip
+powershell -Command Expand-Archive -Force install.zip
+
+if( $TARGET_DIR -eq $null ){
+    mkdir $TARGET_DIR
+}
+
+wsl --import WslServer $TARGET_DIR .\install\install.tar.gz
+wsl -s WslServer
+wsl --upgrade WslServer
+
+:exit
+del install.zip
+rd /s /q install
+pause
+popd
